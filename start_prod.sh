74bc945edad609476923f9f36a47882a6ccae88f
rolleander <roll2@web.de>
2022-04-07 21:23:28 +0200

    - increase nginx post size

--- start_prod.sh
+++ start_prod.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker-compose -f docker-compose.prod.yml up --build
+# stop previous run and remove old containers
+docker-compose -f docker-compose.prod.yml down --rmi all
+# build new containers and start
+docker-compose -f docker-compose.prod.yml up --build -d
