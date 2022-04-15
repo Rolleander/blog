edaa0af49921d51143b4a8d8c4276c70f463aecb
rolleander <roll2@web.de>
2022-04-15 15:47:40 +0200

    - only show published posts

--- app/coreapp/views.py
+++ app/coreapp/views.py
@@ -12,7 +12,7 @@ def listMediaFiles(mediaPath, folder):
 
 
 class BlogView(generic.ListView):
-    queryset = Post.objects.filter(status=0).order_by('-created_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = "posts.html"
 
     def get_context_data(self, **kwargs):
