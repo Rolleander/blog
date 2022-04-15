3f54c180bea0153cc483a7aa26c4fffcc3019f26
rolleander <roll2@web.de>
2022-04-15 10:40:48 +0200

    - remove static dist

--- start_prod.sh
+++ start_prod.sh
@@ -10,3 +10,7 @@ fi
 docker-compose -f docker-compose.prod.yml up --build -d
 # stop pgadmin container (only start on demand)
 ./stop_pgadmin.sh
+if [ "$1" = "clean" ]; then
+    # do migrations
+    docker-compose -f docker-compose.prod.yml exec web python manage.py migrate
+fi
